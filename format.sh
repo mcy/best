56f05cb951b57e7d5ad76f6aa885546fd59c51e9
Miguel Young de la Sota <mcyoung@mit.edu>
2024-06-21 18:16:35 -0400

    fix

--- format.sh
+++ format.sh
@@ -42,42 +42,23 @@ CLANG_FORMAT=$(./bazel cquery \
 echo
 set +e
 
-LICENSE='/* //-*- C++ -*-///////////////////////////////////////////////////////////// *\
-
-  Copyright 2024
-  Miguel Young de la Sota and the Best Contributors 🧶🐈‍⬛
-
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-  use this file except in compliance with the License. You may obtain a copy
-  of the License at
-
-                https://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-  License for the specific language governing permissions and limitations
-  under the License.
-
-\* ////////////////////////////////////////////////////////////////////////// */
-
-'
-LICENSE_LINES=$(wc -l <<< "$LICENSE")
+LICENSE_LINES=$(wc -l < ./LICENSE.inc)
 
 function license() {
   local f=$1
-  if [[ $LICENSE == $(head -n $LICENSE_LINES "$f") ]]; then
-    return 0
-  fi
-  
   if [[ $in_place != 0 ]]; then
     # For all C++ the first interesting line is a #directive.
     local first=$(grep -oahnm 1 "^#" "$f" | grep -oP '\d+')
     local rest=$(tail -n +$first "$f")
-    echo "$LICENSE$rest" > $f
+    cat LICENSE.inc > $f
+    echo >> $f
+    echo "$rest" >> $f
   else
-    echo "$f has incorrect license header"
-    return 1
+    head -n $LICENSE_LINES "$f" |
+    git --no-pager \
+      diff --color=always --no-index --exit-code \
+      ./LICENSE.inc - 
+    return $?
   fi
 }
 
