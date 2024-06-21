823c7d2647528841dde9df83ede691971fc28393
Miguel Young de la Sota <mcyoung@mit.edu>
2024-06-21 16:17:12 -0400

    bytes, spans, oh my

--- best/meta/internal/reflect.h
+++ best/meta/internal/reflect.h
@@ -1,11 +1,87 @@
 #ifndef BEST_META_INTERNAL_REFLECT_H_
 #define BEST_META_INTERNAL_REFLECT_H_
 
+#include <source_location>
+
 #include "best/base/fwd.h"
 #include "best/meta/taxonomy.h"
 #include "best/text/str.h"
 
+// This needs to go in the global namespace, since its full name is relevant for
+// substring operations that extract the names of things. These names are
+// #defined away at the bottom of this header.
+struct BEST_REFLECT_STRUCT_ {
+  BEST_REFLECT_STRUCT_* BEST_REFLECT_FIELD_;
+  enum BEST_REFLECT_ENUM_ { BEST_REFLECT_VALUE_ };
+};
+
 namespace best::reflect_internal {
+// We work exclusively with spans here to avoid pulling in the full machinery
+// of best::str encoding, which is not fast in constexpr.
+template <auto x>
+constexpr best::span<const char> raw_name() {
+  return best::span<const char>::from_nul(
+      std::source_location::current().function_name());
+}
+template <typename x>
+constexpr best::span<const char> raw_name() {
+  return best::span<const char>::from_nul(
+      std::source_location::current().function_name());
+}
+
+// Needles to search for that are *definitely* gonna be in the target compiler's
+// pretty printed function names.
+inline constexpr auto TypeNeedle =
+    best::span<const char>::from_nul("BEST_REFLECT_STRUCT_");
+inline constexpr auto FieldNeedle =
+    best::span<const char>::from_nul("BEST_REFLECT_FIELD_");
+inline constexpr auto ValueNeedle = best::span<const char>::from_nul(
+    "BEST_REFLECT_STRUCT_::BEST_REFLECT_ENUM_::BEST_REFLECT_VALUE_");
+
+struct raw_offsets {
+  size_t start_offset;
+  size_t suffix_len;
+};
+
+// Helpers for creating a structural value that will contain the name of a
+// field symbol.
+template <typename T>
+extern const T v;
+template <typename T>
+struct w {
+  const T* p;
+};
+template <typename T>
+w(const T*) -> w<T>;
+
+// Figure out how the compiler lays out the names of types, fields, and enum
+// values in the names of function templates.
+// inline constexpr auto TypeOffsets = [] {
+//   auto name = raw_name<BEST_REFLECT_STRUCT_>();
+//   auto idx = *name.find(TypeNeedle);
+//   return raw_offsets{
+//       .start_offset = idx,
+//       .suffix_len = name.size() - idx - TypeNeedle.size(),
+//   };
+// }();
+// inline constexpr auto FieldOffsets = [] {
+//   auto name = raw_name<w{&v<BEST_REFLECT_STRUCT_>.BEST_REFLECT_FIELD_}>();
+//   auto idx = *name.find(FieldNeedle);
+//   return raw_offsets{
+//       .start_offset = idx,
+//       .suffix_len = name.size() - idx - FieldNeedle.size(),
+//   };
+// }();
+// inline constexpr auto ValueOffsets = [] {
+//   auto name =
+//       raw_name<BEST_REFLECT_STRUCT_::BEST_REFLECT_ENUM_::BEST_REFLECT_VALUE_>();
+//   auto idx = *name.find(ValueNeedle);
+//   return raw_offsets{
+//       .start_offset = idx,
+//       .suffix_len = name.size() - idx - ValueNeedle.size(),
+//   };
+// }();
+
 enum kind { Field, Struct, Value, Enum, NoFields };
 
 struct validator {
@@ -122,5 +198,10 @@ template <typename T, typename mirror = best::mirror>
 inline constexpr auto info = BestReflect(mirror::BEST_MIRROR_FTADLE_, (T*){});
 
 };  // namespace best::reflect_internal
+
+#define BEST_REFLECT_FIELD_ _private
+#define BEST_REFLECT_STRUCT_ _private
+#define BEST_REFLECT_VALUE_ _private
+#define BEST_REFLECT_ENUM_ _private
 
 #endif  // BEST_META_INTERNAL_REFLECT_H_
