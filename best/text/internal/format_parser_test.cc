d4cb75b4b5f0846a4d0bb371c406e2f3de904106
Miguel Young de la Sota <mcyoung@mit.edu>
2024-06-23 22:42:04 -0400

    get rid of random calls to .fotward()

--- best/text/internal/format_parser_test.cc
+++ best/text/internal/format_parser_test.cc
@@ -67,157 +67,140 @@ best::test ParseOk = [](auto& t) {
                   best::str("}"), best::str("!")});
 
   t.expect_eq(parse("{}"),  //
-              Ast{{best::row{0, format_spec{}}.forward()}});
+              Ast{{best::args(0, format_spec{})}});
 
   t.expect_eq(parse("{} "),  //
-              Ast{{best::row{0, format_spec{}}.forward(), best::str(" ")}});
+              Ast{{best::args(0, format_spec{}), best::str(" ")}});
 
   t.expect_eq(parse(" {}"),  //
-              Ast{{best::str(" "), best::row{0, format_spec{}}.forward()}});
+              Ast{{best::str(" "), best::args(0, format_spec{})}});
 
   t.expect_eq(parse("hello, {}!"),  //
               Ast{{
                   best::str{"hello, "},
-                  best::row{0, format_spec{}}.forward(),
+                  best::args(0, format_spec{}),
                   best::str("!"),
               }});
 
-  t.expect_eq(parse("hello, {}, {}, {}!"),
-              Ast{{
-                  best::str{"hello, "},
-                  best::row{0, format_spec{}}.forward(),
-                  best::str(", "),
-                  best::row{1, format_spec{}}.forward(),
-                  best::str(", "),
-                  best::row{2, format_spec{}}.forward(),
-                  best::str("!"),
-              }});
+  t.expect_eq(parse("hello, {}, {}, {}!"), Ast{{
+                                               best::str{"hello, "},
+                                               best::args(0, format_spec{}),
+                                               best::str(", "),
+                                               best::args(1, format_spec{}),
+                                               best::str(", "),
+                                               best::args(2, format_spec{}),
+                                               best::str("!"),
+                                           }});
 
-  t.expect_eq(parse("hello, {1}, {}, {}!"),
-              Ast{{
-                  best::str{"hello, "},
-                  best::row{1, format_spec{}}.forward(),
-                  best::str(", "),
-                  best::row{0, format_spec{}}.forward(),
-                  best::str(", "),
-                  best::row{1, format_spec{}}.forward(),
-                  best::str("!"),
-              }});
+  t.expect_eq(parse("hello, {1}, {}, {}!"), Ast{{
+                                                best::str{"hello, "},
+                                                best::args(1, format_spec{}),
+                                                best::str(", "),
+                                                best::args(0, format_spec{}),
+                                                best::str(", "),
+                                                best::args(1, format_spec{}),
+                                                best::str("!"),
+                                            }});
 
   t.expect_eq(parse("align: {:x<1} {5:0<1} {:<1} {:<<1} {:x^1} {5:0^1} {:^1} "
                     "{:^^1} {:x>1} "
                     "{5:0>1} {:>1} {:>>1}"),  //
               Ast{{
                   best::str{"align: "},
-                  best::row{0, format_spec{.alignment = format_spec::Left,
-                                           .fill = 'x',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(0, format_spec{.alignment = format_spec::Left,
+                                            .fill = 'x',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{5, format_spec{.alignment = format_spec::Left,
-                                           .fill = '0',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(5, format_spec{.alignment = format_spec::Left,
+                                            .fill = '0',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{1, format_spec{.alignment = format_spec::Left,
-                                           .fill = ' ',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(1, format_spec{.alignment = format_spec::Left,
+                                            .fill = ' ',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{2, format_spec{.alignment = format_spec::Left,
-                                           .fill = '<',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(2, format_spec{.alignment = format_spec::Left,
+                                            .fill = '<',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{3, format_spec{.alignment = format_spec::Center,
-                                           .fill = 'x',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(3, format_spec{.alignment = format_spec::Center,
+                                            .fill = 'x',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{5, format_spec{.alignment = format_spec::Center,
-                                           .fill = '0',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(5, format_spec{.alignment = format_spec::Center,
+                                            .fill = '0',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{4, format_spec{.alignment = format_spec::Center,
-                                           .fill = ' ',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(4, format_spec{.alignment = format_spec::Center,
+                                            .fill = ' ',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{5, format_spec{.alignment = format_spec::Center,
-                                           .fill = '^',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(5, format_spec{.alignment = format_spec::Center,
+                                            .fill = '^',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{6, format_spec{.alignment = format_spec::Right,
-                                           .fill = 'x',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(6, format_spec{.alignment = format_spec::Right,
+                                            .fill = 'x',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{5, format_spec{.alignment = format_spec::Right,
-                                           .fill = '0',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(5, format_spec{.alignment = format_spec::Right,
+                                            .fill = '0',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{7, format_spec{.alignment = format_spec::Right,
-                                           .fill = ' ',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(7, format_spec{.alignment = format_spec::Right,
+                                            .fill = ' ',
+                                            .width = 1}),
                   best::str(" "),
-                  best::row{8, format_spec{.alignment = format_spec::Right,
-                                           .fill = '>',
-                                           .width = 1}}
-                      .forward(),
+                  best::args(8, format_spec{.alignment = format_spec::Right,
+                                            .fill = '>',
+                                            .width = 1}),
               }});
 
-  t.expect_eq(
-      parse("flags: {:#} {:?} {:#?}"),
-      Ast{{
-          best::str{"flags: "},
-          best::row{0, format_spec{.alt = true}}.forward(),
-          best::str(" "),
-          best::row{1, format_spec{.debug = true}}.forward(),
-          best::str(" "),
-          best::row{2, format_spec{.alt = true, .debug = true}}.forward(),
-      }});
+  t.expect_eq(parse("flags: {:#} {:?} {:#?}"),
+              Ast{{
+                  best::str{"flags: "},
+                  best::args(0, format_spec{.alt = true}),
+                  best::str(" "),
+                  best::args(1, format_spec{.debug = true}),
+                  best::str(" "),
+                  best::args(2, format_spec{.alt = true, .debug = true}),
+              }});
 
   t.expect_eq(
       parse("widths: {:5} {:05} {:<5}"),
       Ast{{
           best::str{"widths: "},
-          best::row{0, format_spec{.width = 5}}.forward(),
+          best::args(0, format_spec{.width = 5}),
           best::str(" "),
-          best::row{1, format_spec{.sign_aware_padding = true, .width = 5}}
-              .forward(),
+          best::args(1, format_spec{.sign_aware_padding = true, .width = 5}),
           best::str(" "),
-          best::row{2, format_spec{.alignment = format_spec::Left, .width = 5}}
-              .forward(),
+          best::args(2,
+                     format_spec{.alignment = format_spec::Left, .width = 5}),
       }});
+
+  t.expect_eq(parse("precs: {:.2} {:5.2} {:05.2}"),
+              Ast{{
+                  best::str{"precs: "},
+                  best::args(0, format_spec{.prec = 2}),
+                  best::str(" "),
+                  best::args(1, format_spec{.width = 5, .prec = 2}),
+                  best::str(" "),
+                  best::args(2, format_spec{.sign_aware_padding = true,
+                                            .width = 5,
+                                            .prec = 2}),
+              }});
 
   t.expect_eq(
-      parse("precs: {:.2} {:5.2} {:05.2}"),
+      parse("methods: {:x} {:o} {:A} {:x?}"),
       Ast{{
-          best::str{"precs: "},
-          best::row{0, format_spec{.prec = 2}}.forward(),
+          best::str{"methods: "},
+          best::args(0, format_spec{.method = rune('x')}),
           best::str(" "),
-          best::row{1, format_spec{.width = 5, .prec = 2}}.forward(),
+          best::args(1, format_spec{.method = rune('o')}),
           best::str(" "),
-          best::row{
-              2, format_spec{.sign_aware_padding = true, .width = 5, .prec = 2}}
-              .forward(),
+          best::args(2, format_spec{.method = rune('A')}),
+          best::str(" "),
+          best::args(3, format_spec{.debug = true, .method = rune('x')}),
       }});
-
-  t.expect_eq(parse("methods: {:x} {:o} {:A} {:x?}"),
-              Ast{{
-                  best::str{"methods: "},
-                  best::row{0, format_spec{.method = rune('x')}}.forward(),
-                  best::str(" "),
-                  best::row{1, format_spec{.method = rune('o')}}.forward(),
-                  best::str(" "),
-                  best::row{2, format_spec{.method = rune('A')}}.forward(),
-                  best::str(" "),
-                  best::row{3, format_spec{.debug = true, .method = rune('x')}}
-                      .forward(),
-              }});
 };
 }  // namespace best::format_internal::parser_test
